2e2f0738979a7d7607135dbf0bad6b5bfa916373
Davide Bolcioni <dbolcioni@users.noreply.github.com>
2019-10-28 22:30:37 -0700

    Add die() function

--- src/lib/util-helpers.bash
+++ src/lib/util-helpers.bash
@@ -1,3 +1,4 @@
 # Assorted helpers for Bash utility scripts
 
-warn() { echo "${0##*/}:" "${@}" 1>&2 ; }
+warn() { echo "${0##*/}:" "$@" 1>&2 ; }
+die() { local rc="$1"; shift; warn "$@"; exit "$rc"; }
