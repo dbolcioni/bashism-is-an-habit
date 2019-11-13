4e7cfd9b14b1cdbd8d0c1d0713c3f62f26e40655
Davide Bolcioni <dbolcioni@users.noreply.github.com>
2019-11-12 16:34:17 -0800

    Sidestep use of local in die()

--- src/lib/util-helpers.bash
+++ src/lib/util-helpers.bash
@@ -1,4 +1,4 @@
 # Assorted helpers for Bash utility scripts
 
 warn() { echo "${0##*/}:" "$@" 1>&2 ; }
-die() { local rc="$1"; shift; warn "$@"; exit "$rc"; }
+die() { (shift; warn "$@"); exit "$1"; }
